74b0ba99e2a9d663a8e16336f444a750d208f6ac
padifa <padifa7@live.fr>
2024-10-17 15:43:09 -0500

    product can now be added to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -26,15 +26,15 @@ function Cart() {
     history.push("/");
   };
 
-  //   const handleType = (event) => {
-  //     console.log("type selection", event.target.id);
-  //     setType(event.target.id);
-  //   };
+  const handleType = (event) => {
+    console.log("type selection", event.target.id);
+    setType(event.target.id);
+  };
   return (
     <>
       <div className="customer-info" style={{ backgroundColor: "#11ee52" }}>
         <h2>Order List</h2>
-        {JSON.stringify(product)}
+        {JSON.stringify(products)}
         <ul>
           <li>Username: {customer.username}</li>
           <li>Phone Number: {customer.phone_number}</li>
@@ -64,6 +64,13 @@ function Cart() {
           </tbody>
         </table>
         <label>Total:</label>
+        <button type="button" id="delivery" onClick={handleType}>
+          Delivery
+        </button>
+        <button type="button" id="pickup" onClick={handleType}>
+          Pickup
+        </button>
+
         <button onClick={handleCart}>Checkout</button>
       </div>
     </>
@@ -71,86 +78,3 @@ function Cart() {
 }
 
 export default Cart;
-
-//   console.log(orders);
-//   let foundProducts = [];
-//   for (let product of orders) {
-//     for (let ordered of products) {
-//       if (product.id === ordered.id) {
-//         foundProducts.push(ordered);
-//       }
-//     }
-//   }
-//   console.log(foundProducts);
-//   let totalCost = 0;
-//   for (let cost of foundProducts) {
-//     const price = parseFloat(cost.price) || 0;
-//     if (price > 0) {
-//       totalCost += price;
-//     }
-//   }
-//   //   ("username", "password", "phone_number", "role", "created_at", "isAdmin", "email", "address", "farm_name")
-//   totalCost = totalCost.toFixed(2);
-//   const [id, setId] = useState("");
-//   const [username, setUsername] = useState("");
-//   const [phone_number, setPhoneNumber] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [address, setAddress] = useState("");
-//   const [city, setCity] = useState("");
-//   const [country, setCountry] = useState("");
-//   const [zip, setZip] = useState("");
-
-//   const handleCart = () => {
-//     const newOrder = {
-//       username: customer.username,
-//       phone_number: customer.phone_number,
-//       email: customer.email,
-//       address: customer.address,
-//       city: customer.city,
-//       country: customer.country,
-//       zip: customer.zip,
-//       products: customer.order,
-//       total: totalCost,
-//     };
-//     dispatch(cartRequest(newOrder));
-//     history.push("/");
-//   };
-//   return (
-//     <>
-//       <div className="customer-info">
-//         <ul>
-//           <li>UserName: {customer.username}</li>
-//           <li>Phone_number: {customer.phone.number}</li>
-//           <li>E-mail: {customer.email}</li>
-//           <li>Address: {customer.address}</li>
-//           <li>City: {customer.city}</li>
-//           <li>Country: {customer.country}</li>
-//           <li>Zip: {customer.zip}</li>
-//         </ul>
-//       </div>
-//       <div className="cart_container">
-//         <h2>Cart</h2>
-//         <table className="cart_table">
-//           <thead>
-//             <tr>
-//               <th>Product</th>
-//               <th>Price</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {foundProducts.map((item, id) => (
-//               <tr key={id}>
-//                 <td>{item.name}</td>
-//                 <td>{item.price}</td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//         <label>Total:</label>
-//         <p>${totalCost}</p>
-//         <button onClick={handleCart}>Cart</button>
-//       </div>
-//     </>
-//   );
-// }
-// export default Cart;
