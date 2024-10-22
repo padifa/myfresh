2fae1e1e531a2298b351bb7dec54df2bbc6f30d4
padifa <padifa7@live.fr>
2024-10-22 14:50:04 -0500

    Homepage styled

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,67 +1,74 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import './LandingPage.css';
-
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
 // CUSTOM COMPONENTS
-import RegisterForm from '../RegisterForm/RegisterForm';
+import RegisterForm from "../RegisterForm/RegisterForm";
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState("Welcome");
   const history = useHistory();
 
-  const onLogin = (event) => {
-    history.push('/login');
+  const onLogin = () => {
+    history.push("/login");
   };
 
   return (
-    <div className="container">
-      <h2>{heading}</h2>
-
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
-          </p>
-
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
-      </div>
+    <div
+      style={{
+        backgroundImage: `url('/public/images/organic-products.jpg')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Container className="text-black py-5">
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <Card>
+              <Card className="body">
+                <h2>{heading}</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Curabitur id felis metus. Vestibulum et pulvinar tortor. Morbi
+                  pharetra lacus ut ex molestie blandit. Etiam et turpis sit
+                  amet risus mollis interdum. Suspendisse et justo vitae metus
+                  bibendum fringilla sed sed justo. Aliquam sollicitudin dapibus
+                  lectus, vitae consequat odio elementum eget.
+                </p>
+                <p>
+                  Praesent consectetur orci dui, id elementum eros facilisis id.
+                  Sed id dolor in augue porttitor faucibus eget sit amet ante.
+                  Nunc consectetur placerat pharetra. Aenean gravida ex ut erat
+                  commodo, ut finibus metus facilisis.
+                </p>
+                <p>
+                  Fusce porta diam ac tortor elementum, ut imperdiet metus
+                  volutpat. Suspendisse posuere dapibus maximus. Aliquam vitae
+                  felis libero. In vehicula sapien at semper ultrices.
+                </p>
+              </Card>
+            </Card>
+          </Col>
+          <Col md={4} className="d-flex flex-column align-items-center">
+            <Card>
+              <Card className="body">
+                <RegisterForm />
+                <h4 className="mt-4">Already a Member?</h4>
+                <Button
+                  variant="primary"
+                  size="sm"
+                  onClick={onLogin}
+                  className="mt-2"
+                >
+                  Login
+                </Button>
+              </Card>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,82 +3,90 @@ import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
 import { useDispatch } from "react-redux";
-import { Container, Button, ListGroup, Row, Col } from "react-bootstrap";
+import { Container, Button, ListGroup, Row, Col, Card } from "react-bootstrap";
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
+
   const user = useSelector((store) => store.user);
   const products = useSelector((store) => store.product);
   const history = useHistory();
   const dispatch = useDispatch();
-  console.log("products", products);
+
   const myProducts = products.filter(
     (product) => Number(product.user_id) === Number(user.id)
   );
-  console.log("My products", myProducts);
 
   const handleEdit = (productId) => {
     history.push(`/edit/productForm/${productId}`);
-    console.log("edit this product", productId);
   };
+
   const handleDelete = (productId) => {
     dispatch({
       type: "DELETE_PRODUCT",
       payload: productId,
     });
   };
-  const addProduct = (event) => {
-    console.log("add new product");
+
+  const addProduct = () => {
     history.push(`/productForm`);
   };
+
   return (
     <Container className="mt-5">
       <h2 className="mb-4">Welcome, {user.username}!</h2>
+
       {user.role === "farmer" && (
         <Button variant="success" onClick={addProduct} className="mb-3">
           Add Product
         </Button>
       )}
+
       <p>Your ID is: {user.id}</p>
 
-      <ListGroup>
+      {/* Display the products in card layout */}
+      <Row>
         {myProducts?.map((product) => (
-          <ListGroup.Item key={product.id}>
-            <Row>
-              <Col xs={8}>
-                <h5>{product.name}</h5>
-                <p>
-                  Category: {product.category} | Price: ${product.price}
+          <Col md={4} sm={6} xs={12} className="mb-4" key={product.id}>
+            <Card>
+              <Card.Img
+                variant="top"
+                src={product.image_url || "public/Images/apple.jpeg"}
+                alt={product.name}
+                style={{ height: "200px", objectFit: "cover" }}
+              />
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>
+                  <strong>Category:</strong> {product.category}
+                  <br />
+                  <strong>Price:</strong> ${product.price}
                   <br />
                   {product.description}
-                </p>
-              </Col>
-              <Col
-                xs={4}
-                className="d-flex justify-content-end align-items-center"
-              >
-                <Button
-                  variant="primary"
-                  onClick={() => handleEdit(product.id)}
-                  className="me-2"
-                >
-                  Edit
-                </Button>
-                <Button
-                  variant="danger"
-                  onClick={() => handleDelete(product.id)}
-                >
-                  Remove
-                </Button>
-              </Col>
-            </Row>
-          </ListGroup.Item>
+                </Card.Text>
+                <div className="d-flex justify-content-between">
+                  <Button
+                    variant="primary"
+                    onClick={() => handleEdit(product.id)}
+                    className="me-2"
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    variant="danger"
+                    onClick={() => handleDelete(product.id)}
+                  >
+                    Remove
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </ListGroup>
+      </Row>
 
       <LogOutButton className="btn mt-4" />
     </Container>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
