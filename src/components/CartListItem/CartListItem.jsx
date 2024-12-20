e83583498fb2701a535e0a244918a3398acc06f0
padifa <padifa7@live.fr>
2024-12-19 21:10:31 -0600

    CartListItem commented

--- src/components/CartListItem/CartListItem.jsx
+++ src/components/CartListItem/CartListItem.jsx
@@ -4,22 +4,31 @@ import { useDispatch } from "react-redux";
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name TemplateFunction with the name for the new component.
+
+// Functional component to display an individual cart item
 function CartListItem({ item }) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
-  const dispatch = useDispatch();
-  console.log("product item", item);
+
+  const dispatch = useDispatch(); // Hook to dispatch actions to Redux store
+
+  console.log("product item", item); // Debug log to check the item passed as props
+
+  // Function to handle the removal of an item from the cart
   const handleDelete = (itemId) => {
+    // Dispatch an action to remove the product from the cart
     dispatch({ type: "REMOVE_PRODUCT_FROM_CART", payload: itemId });
   };
 
   return (
     <tr>
-      <td>{item?.name}</td>
-      <td>{item?.price}</td>
-      <td>{item?.quantity}</td>
-      <td>${item?.price * item?.quantity}</td>
+      {/* Displaying product details in table cells */}
+      <td>{item?.name}</td> {/* Product name */}
+      <td>{item?.price}</td> {/* Product price */}
+      <td>{item?.quantity}</td> {/* Quantity of product in the cart */}
+      <td>${item?.price * item?.quantity}</td> {/* Total price for this item */}
       <td>
+        {/* Button to remove the item, styled with Bootstrap */}
         <Button variant="danger" onClick={() => handleDelete(item?.id)}>
           Remove
         </Button>
@@ -29,3 +38,13 @@ function CartListItem({ item }) {
 }
 
 export default CartListItem;
+
+/*
+Summary of the Component:
+1. `CartListItem` is a functional component that represents a single row in a shopping cart table.
+2. It receives `item` as a prop, which contains the product details (name, price, quantity, etc.).
+3. The `handleDelete` function dispatches a Redux action to remove the item from the cart.
+4. Bootstrap is used for styling the "Remove" button with a red (danger) variant.
+5. The component is designed to be reusable for any cart item passed to it as a prop.
+6. The `console.log` helps during development to confirm the data being passed to this component.
+*/
